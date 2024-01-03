0760f635da708cf58e1dd0472743d4d49cac0afe
Alejandro <smarketing.cuz@gmail.com>
2024-01-02 21:41:38 -0500

    Refactor speechToText function to accept both File and Buffer inputs

--- app/api/speech-to-text/route.ts
+++ app/api/speech-to-text/route.ts
@@ -2,12 +2,10 @@ import { speechToText } from "@/lib/speech-to-text";
 
 export async function POST(req: Request) {
   const audioBlob = await req.blob();
-  const audioFile = new File([audioBlob], "tempSpeech.m4a", {
-    type: "audio/m4a",
-  });
+  const audioBuffer = Buffer.from(await audioBlob.arrayBuffer());
 
   try {
-    const result = await speechToText(audioFile);
+    const result = await speechToText(audioBuffer);
     return Response.json({ message: "ok", result });
   } catch (error) {
     return Response.json({ message: "error", error });
