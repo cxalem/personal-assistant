8f86e4784eb35cccdd6eac413df8dc6a64744857
Alejandro <smarketing.cuz@gmail.com>
2024-01-07 23:15:07 -0500

    Add Avatar component to Chat component

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -2,6 +2,7 @@
 
 import { useCallback, useMemo, useRef, useState } from "react";
 import { Message } from "./Message";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 type Question = {
   question: string;
@@ -108,9 +109,22 @@ export const Chat = () => {
   return (
     <div
       ref={chatRef}
-      className="relative bg-white shadow-md flex flex-col gap-4 shadow-gray-100 rounded-xl md:px-10 md:pt-10 w-[90vw] overflow-scroll h-[50vh] md:w-[60vw] mx-auto"
+      className="relative bg-orange-50/70 shadow-md flex flex-col gap-4 shadow-gray-100 rounded-xl w-[90vw] overflow-scroll h-[50vh] md:w-[50vw] mx-auto"
     >
-      <div className="flex flex-col gap-4 flex-1">
+      <div className="sticky top-0 z-10 md:px-10 md:py-3 flex gap-4 bg-white shadow-lg shadow-neutral-400/10 items-center">
+        <Avatar>
+          <AvatarImage
+            src="https://www.notion.so/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ff4b39258-8c2f-46b2-bf6e-5ed2bbe98d93%2F902166bb-984f-4be9-925f-0deed31d69c1%2FIcono_Dev.svg?table=block&id=7baf963b-cdab-4cb8-a1d4-f878776a2a66&spaceId=f4b39258-8c2f-46b2-bf6e-5ed2bbe98d93&userId=b5d9e56c-d386-48d8-bfda-deb6eaf84065&cache=v2"
+            alt="@_cxalem"
+          />
+          <AvatarFallback>TR</AvatarFallback>
+        </Avatar>
+        <div className="flex flex-col">
+          <span className="text-lg font-medium">Travis</span>
+          <span className="text-sm opacity-30">Online</span>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 flex-1 md:px-10 ">
         {questions.map(({ question, answer, answered }) => {
           if (!answered) return null;
           return (
@@ -121,7 +135,8 @@ export const Chat = () => {
           );
         })}
       </div>
-      <div className="sticky bg-white md:py-4 rounded-b-xl h-full max-h-[164px] z-10 bottom-0 grid grid-cols-2 w-full gap-4">
+
+      <div className="sticky bg-white border-t border-gray-100 md:py-4 md:px-10 rounded-b-xl h-full max-h-[164px] z-10 bottom-0 grid grid-cols-2 w-full gap-4">
         {questionButtons}
       </div>
     </div>
